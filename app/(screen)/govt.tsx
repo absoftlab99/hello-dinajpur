fe925569f57967d60a6389a7a654d7eff32c4a22
absoftlab99 <abdullahtech2@outlook.com>
2025-01-18 13:50:36 +0600

    hello Dinajpur

---
+++ app/(screen)/govt.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Govt = () => {
+    return (
+        <View>
+            <Text>Governemnt</Text>
+        </View>
+    )
+}
+
+export default Govt
---
+++ app/+not-found.tsx
@@ -0,0 +1,24 @@
+import { Link, Stack } from 'expo-router';
+import { StyleSheet, View } from 'react-native';
+
+export default function NotFoundScreen() {
+  return (
+    <View>
+      <Stack.Screen options={{ title: 'Oops!' }} />
+      <Link href='/'>Go To Home</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+});
